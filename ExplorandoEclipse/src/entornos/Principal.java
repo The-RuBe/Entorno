be47cd1232a2ac0c703bc9fe76df600bd405e03d
The-RuBe <rubenroldanjimenez12@gmail.com>
2025-10-23 20:15:11 +0200

    UpdatePrincipal

--- ExplorandoEclipse/src/entornos/Principal.java
+++ ExplorandoEclipse/src/entornos/Principal.java
@@ -1,9 +1,11 @@
 package entornos;
 
-public class Principal {
+//	He cambiado el tema a dark aunque era el que estaba por defecto del sistema, y he añadido la fuente "Verdana" 
+//	al Java Editor Text Font, le cambiado el tamaño a 12 y la he puesto en negrita.
 
+public class Principal {
 	public static void main(String[] args) {
- System.out.println("Bienvenido a Eclipse");
+ System.out.print("Bienvenido a Eclipse");
  System.out.println("Vamos a conocer el entorno de desarrollo");
- }
+	}
 }
