57aa0512acd6ba23fa985c9fa1d9044714799b64
The-RuBe <rubenroldanjimenez12@gmail.com>
2025-11-03 22:21:49 +0100

    Casi terminado

--- DepuracionEclipse/src/ejercicio2/FactorialApp.java
+++ DepuracionEclipse/src/ejercicio2/FactorialApp.java
@@ -1,13 +1,28 @@
-public class FactorialApp { 
- public static void main(String[] args) { 
- int numero = 5; 
- int resultado = factorial(numero); 
- System.out.println("El factorial de " + numero + " es " + resultado);  } 
- public static int factorial(int n) { 
- int fact = 0; 
- for (int i = 1; i <= n; i--) { 
- fact = fact * i; 
- } 
- return fact; 
- } 
+package ejercicio2;
+
+public class FactorialApp {
+	public static void main(String[] args) {
+		int numero = 5;
+		int resultado = factorial(numero);
+		if (resultado != -1) {
+			System.out.println("El factorial de " + numero + " es " + resultado);
+		}
+	}
+
+	public static int factorial(int n) {
+		if (n < 0) {
+			System.out.println("Error: no existe el factorial de un número negativo.");
+			return -1;
+		}
+
+		if (n == 0) {
+			return 1;
+		}
+
+		int fact = 1;
+		for (int i = 1; i <= n; i++) {
+			fact = fact * i;
+		}
+		return fact;
+	}
 }
